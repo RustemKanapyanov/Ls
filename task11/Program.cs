52e9b32df5ac291e9405ac21760af5bbe859394b
rustem <tkanapyanov@gmail.com>
2022-08-13 19:09:45 +0600

    add dz seminar 2

---
+++ task11/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 11: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(Convert.ToString((a / 100))+Convert.ToString((a % 10)))
