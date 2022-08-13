c80329a8b9dd028cae8d48e6b488147ff7345637
rustem <tkanapyanov@gmail.com>
2022-08-13 19:45:10 +0600

    add ; fix to eror

--- task11/Program.cs
+++ task11/Program.cs
@@ -5,4 +5,4 @@
 
 Console.WriteLine("Введите число ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine(Convert.ToString((a / 100))+Convert.ToString((a % 10)))
+Console.WriteLine(Convert.ToString((a / 100))+Convert.ToString((a % 10)));
