be3626bb96674959c8ab05c24d084caed820f47c
rustem <tkanapyanov@gmail.com>
2022-08-14 19:41:47 +0600

    add task26 and fix task16

--- task16/Program.cs
+++ task16/Program.cs
@@ -7,7 +7,7 @@
 Console.WriteLine("Введите первое число");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите второе число");
-int b=Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
     if ((a * a == b)^(b * b == a))
 {
 Console.WriteLine("Да");
