486239e7a924f3011c368027ca038275e76ac19a
rustem <tkanapyanov@gmail.com>
2022-08-15 22:23:28 +0600

    add task17

--- task13/Program.cs
+++ task13/Program.cs
@@ -6,13 +6,48 @@
 
 // 32679 -> 6
 
-Console.WriteLine("Введите число ");
-int a = Convert.ToInt32(Console.ReadLine());
-    if (a > 99)
+// Console.WriteLine("Введите число ");
+// int a = Convert.ToInt32(Console.ReadLine());
+//     if (a > 99)
+// {
+// Console.WriteLine(Convert.ToString(a)[2]);
+// }
+//     else
+// {
+//     Console.WriteLine("Третьей цифры нет");
+// }
+
+// Console.WriteLine("Введите число ");
+// int number = Convert.ToInt32(Console.ReadLine());
+//     number = Math.Abs(number);
+//     if (number < 100)
+//     {
+//         Console.WriteLine("В числе нет третьей цифры");
+//     }
+//     else
+//     {
+//         while(number >= 1000)
+//         {
+//             number /= 10;
+//         }
+//         int digit3 = number % 10;
+//         Console.WriteLine(digit3);
+//     }
+    
+
+Console.Write("Введите число ");
+string numberStr = Console.ReadLine();
+int digitIndex =2;
+
+if (numberStr[0] == '-')
 {
-Console.WriteLine(Convert.ToString(a)[2]);
+    digitIndex++;
 }
-    else
+if (numberStr.Length < digitIndex + 1)
 {
-    Console.WriteLine("Третьей цифры нет");
+    Console.WriteLine("В числе нет третьей цифры");
+}
+else
+{ 
+    Console.WriteLine(numberStr[2]);
 }
