0935ad606b26b52e3e1539f28a3c4266fc19e6f1
rustem <tkanapyanov@gmail.com>
2022-08-19 00:58:07 +0600

    add task23

--- task23/Program.cs
+++ task23/Program.cs
@@ -1,11 +1,30 @@
-﻿// Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до (N)
+﻿//Задача 23: Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
-Console.Write("Введите Число: ");
- int N = Convert.ToInt32(Console.ReadLine());
- int i = 1;
- while (i <= N)
- {
-    double res = Math.Pow(13);
-    Console.WriteLine(res);
-    i++;
- }
+Console.WriteLine("\n Задача 23");
+Console.Write("Введите число: ");
+int cube = Convert.ToInt32(Console.ReadLine());
+
+void Cube(int[] cube)
+{
+  int counter = 0;
+  int length = cube.Length;
+  while (counter <  length)
+  {
+    cube[counter] = Convert.ToInt32(Math.Pow(counter, 3));
+    counter++;
+  }
+}
+void PrintArry(int[] coll){
+  int count = coll.Length;
+  int index = 0;
+  while(index < count){
+    Console.Write(coll[index]+ " ");
+    index++;
+  }
+} 
+
+int[] arry = new int[cube+1];
+Cube(arry);
+PrintArry(arry);
