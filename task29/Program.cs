cda4a37ac414810b431dd82fd86d82c43d9b07b1
rustem <tkanapyanov@gmail.com>
2022-08-19 00:58:58 +0600

    add task29

---
+++ task29/Program.cs
@@ -0,0 +1,11 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.Clear();
+Console.WriteLine("Массив из 8 случайны чисел ");
+Random rnd = new Random();
+int[] arr = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    arr[i] = rnd.Next();
+    Console.WriteLine(arr[i]); 
+}
